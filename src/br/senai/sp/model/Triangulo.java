d3b352b4bbfc991e160a6fb4a0edf0c1e39a527f
PIETRA PAULA VOLPATO <23111825@CFP127ALUNO.LOCAL>
2023-03-01 12:17:00 -0300

    Aula 01-03-2023 - Triangulo

---
+++ src/br/senai/sp/model/Triangulo.java
@@ -0,0 +1,36 @@
+package br.senai.sp.model;
+
+public class Triangulo {
+
+	// Propiedades/Atribuidos da classe
+	public double base; 
+	public double ladoB;
+	public double ladoC;
+	public double altura;
+	public String nome;
+	
+	//Métodos da classe
+	public void mostrarDimensoes() {
+		
+		System.out.println(" - - - - - - -  - - - - ");
+		System.out.println(nome);
+		System.out.println(" - - - - - - - - - - - -");
+		System.out.println("Base-----> " + base);
+		System.out.println("Lado B-----> " + ladoB);
+		System.out.println("Lado C-----> " + ladoC);
+		System.out.println("Altura-----> " + altura);
+		calcularArea();
+	}
+		
+ 
+		public void calcularArea() {
+			
+			double area = (base * altura) / 2; 
+			System.out.println("Área-----> " + area);
+		
+		}
+		
+	}
+	
+	
+
